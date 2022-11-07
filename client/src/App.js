c5157a5b1dbad1b7d8058560467492b8223cc53b
Aziz Limonu <azizlimonu@gmail.com>
2022-11-07 11:04:04 +0800

    adding feature, fix soon

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react"
+import { Footer } from "./components/footer/Footer"
+import { Header } from "./components/header/Header"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { Regsiter } from "./pages/login/Regsiter"
+import { Routes, Route } from "react-router-dom"
+import { DetailsPages } from "./pages/details/DetailsPages"
+import { Account } from "./pages/account/Account"
+import { Create } from "./components/create/Create"
+import { Context } from "./context/Context"
 
-function App() {
+const App = () => {
+  //after login
+  const { user } = useContext(Context)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Regsiter />} />
+        <Route path="/post/:id" element={<DetailsPages />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
-
-export default App;
+export default App
--- client/src/assets/data/data.js
+++ client/src/assets/data/data.js
@@ -1,3 +1,13 @@
+import blog1 from '../images/post-astronomy-(4).jpg';
+import blog2 from '../images/post-astronomy-(5).jpg';
+import blog3 from '../images/post-food-(3).jpg';
+import blog4 from '../images/post-life-(3).jpg';
+import blog5 from '../images/post-life-(4).jpg';
+import blog6 from '../images/post-love-(2).jpg';
+import blog7 from '../images/post-love-(3).jpg';
+import blog8 from '../images/post-tour-(5).jpg';
+import blog9 from '../images/product1.jpg';
+
 export const nav = [
   {
     id: 1,
@@ -30,49 +40,49 @@ export const category = [
     id: 1,
     category: "Life",
     title: "Stay Calm And Surf",
-    cover: "../images/category/ca1.png",
+    cover: blog1,
   },
   {
     id: 2,
     category: "Fashion",
     title: "Becoming a Dragonfly",
-    cover: "../images/category/ca2.png",
+    cover: blog2,
   },
   {
     id: 3,
     category: "Travel",
     title: "There's always light at the end of the tunnel",
-    cover: "../images/category/ca3.png",
+    cover: blog8,
   },
   {
     id: 4,
     category: "Sport",
     title: "Stay Calm And Surf",
-    cover: "../images/category/ca4.png",
+    cover: blog3,
   },
   {
     id: 5,
     category: "Fun",
     title: "There's always light at the end of the tunnel",
-    cover: "../images/category/ca5.png",
+    cover: blog4,
   },
   {
     id: 6,
     category: "Health",
     title: "Becoming a Dragonfly",
-    cover: "../images/category/ca6.png",
+    cover: blog6,
   },
   {
     id: 7,
     category: "Business",
     title: "Stay Calm And Surf",
-    cover: "../images/category/ca7.png",
+    cover: blog5,
   },
   {
     id: 8,
     category: "Technology",
     title: "There's always light at the end of the tunnel",
-    cover: "../images/category/ca8.png",
+    cover: blog7,
   },
 ]
 export const blog = [
@@ -81,7 +91,7 @@ export const blog = [
     title: "POST NO.1 CONTAINING A YOUTUBE VIDEO",
     desc: "Quisque at tellus nec magna viverra euismod. Maecenas lobortis, nisl et sodales blandit, neque mi ullamcorper eros, id efficitur ex velit in ante. Suspendisse elementum congue lacus quis gravida. Nunc eu hendrerit risus. Quisque blandit, orci non maximus vulputate, ligula ante ornare enim, ac interdum lorem leo varius mauris. Aliquam non sem vel orci malesuada sagittis. Vivamus id ullamcorper tellus. In hac habitasse platea dictumst. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et sapien a arcu feugiat mattis. Suspendisse sed magna quis turpis rhoncus venenatis.",
     category: "Nature",
-    cover: "../images/blogs/b1.jpg",
+    cover: blog1,
     date: "APRIL 05, 2018",
   },
   {
@@ -89,7 +99,7 @@ export const blog = [
     title: "JUPITER IS FULL OF WATERFALLS SO EARLY",
     desc: "Vestibulum suscipit nibh sed odio pharetra, condimentum lacinia nisi varius. Ut ac ornare diam. Suspendisse vitae quam risus. Nulla ullamcorper libero justo, vitae dictum est efficitur id. Aenean et lacinia arcu. Sed non dolor id risus dapibus congue. Quisque tincidunt vitae tellus ut malesuada. Nulla eleifend nibh a maximus consectetur. Duis et mi porta, bibendum leo non, fermentum enim. Vivamus sed diam suscipit, ultricies ipsum sed, efficitur dui. Sed pharetra purus at iaculis vehicula. Fusce pellentesque placerat ante, id elementum dolor maximus sed. Aenean viverra at lectus quis vehicula. Aenean at arcu odio. Etiam mauris justo, cursus finibus aliquam eget, fermentum nec nulla. Sed finibus viverra magna non sagittis. Mauris fringilla augue vel quam rhoncus, in tempor enim lacinia. Donec nibh velit, scelerisque id neque sit amet, ornare consectetur sem. Donec convallis nec justo id consequat. Donec sem leo, pulvinar sit amet nisi a, mollis gravida est. Duis porttitor eleifend cursus. Vivamus ac dapibus dolor. Nullam leo tellus, rhoncus sit amet tempor vel, malesuada vel augue. Morbi laoreet sem eget tortor pulvinar mattis. Maecenas commodo ultricies orci, ut blandit urna accumsan sit amet. Ut a porttitor orci, maximus feugiat turpis. In eu sodales orci, et volutpat felis. Quisque orci quam, gravida at nulla tempor, vulputate pretium magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent congue, magna vitae interdum blandit, arcu mauris placerat nulla, sit amet rhoncus justo risus in neque.",
     category: "Nature",
-    cover: "../images/blogs/b2.jpg",
+    cover: blog2,
     date: "MARCH 15, 2018",
   },
   {
@@ -97,7 +107,7 @@ export const blog = [
     title: "START YOU CUP ON TABLE OF THAT",
     desc: "Duis rutrum eu tortor et efficitur. Nulla ac consequat leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut vestibulum mattis est eu volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae tristique nibh. Quisque vel lectus interdum lacus ultricies venenatis. In ex nisl, commodo vitae risus ac, efficitur imperdiet dolor. Maecenas dignissim luctus mi sit amet mollis. Maecenas sit amet bibendum dolor, a molestie tellus. Donec et varius risus. Duis tincidunt mi lorem, vitae luctus turpis rutrum sit amet. Aliquam aliquet malesuada odio, ut pellentesque nisi tempus in. Ut fermentum, augue id accumsan aliquet, dolor dui euismod elit, nec fringilla mi justo nec mauris. Duis tellus nisl, elementum et urna at, molestie condimentum eros. Sed mattis quis risus id tincidunt. Donec ultrices vestibulum vestibulum.",
     category: "Sports",
-    cover: "../images/blogs/b3.jpg",
+    cover: blog3,
     date: "MARCH 15, 2018",
   },
   {
@@ -105,7 +115,7 @@ export const blog = [
     title: "START YOU CUP ON TABLE OF THAT",
     desc: "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition.",
     category: "Fashion",
-    cover: "../images/blogs/b4.jpg",
+    cover: blog4,
     date: "February 20, 2018",
   },
   {
@@ -113,7 +123,7 @@ export const blog = [
     title: "SMART TYPE OF GAMING WITH CO-OP",
     desc: "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition.",
     category: "Sports",
-    cover: "../images/blogs/b5.jpg",
+    cover: blog4,
     date: "February 20, 2018",
   },
   {
@@ -121,7 +131,7 @@ export const blog = [
     title: "TEACH YOUR DRAGON HOW TO FLY",
     desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
     category: "Fashion",
-    cover: "../images/blogs/b6.jpg",
+    cover: blog5,
     date: "February 20, 2018",
   },
   {
@@ -129,7 +139,7 @@ export const blog = [
     title: "TEACH YOUR DRAGON HOW TO FLY",
     desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
     category: "Fashion",
-    cover: "../images/blogs/b7.jpg",
+    cover: blog6,
     date: "February 20, 2018",
   },
   {
@@ -137,7 +147,7 @@ export const blog = [
     title: "TEACH YOUR DRAGON HOW TO FLY",
     desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
     category: "Fashion",
-    cover: "../images/blogs/b8.jpg",
+    cover: blog6,
     date: "February 20, 2018",
   },
   {
@@ -145,7 +155,7 @@ export const blog = [
     title: "TEACH YOUR DRAGON HOW TO FLY",
     desc: "Magnis modipsae que lib voloratati andigen daepedor quiate ut reporemni aut labor. Laceaque quiae sitiorem ut restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp dolupta orem retibusam emnis et consent accullignis lomnus. We don't want to overload you with hundreds of styles you don't want, or need. We give you a strong base to express your own creativity.",
     category: "Fashion",
-    cover: "../images/blogs/b9.jpg",
+    cover: blog7,
     date: "February 20, 2018",
   },
   {
@@ -153,7 +163,7 @@ export const blog = [
     title: "POST NO.1 CONTAINING A YOUTUBE VIDEO",
     desc: "Quisque at tellus nec magna viverra euismod. Maecenas lobortis, nisl et sodales blandit, neque mi ullamcorper eros, id efficitur ex velit in ante. Suspendisse elementum congue lacus quis gravida. Nunc eu hendrerit risus. Quisque blandit, orci non maximus vulputate, ligula ante ornare enim, ac interdum lorem leo varius mauris. Aliquam non sem vel orci malesuada sagittis. Vivamus id ullamcorper tellus. In hac habitasse platea dictumst. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et sapien a arcu feugiat mattis. Suspendisse sed magna quis turpis rhoncus venenatis.",
     category: "Nature",
-    cover: "../images/category/ca1.png",
+    cover: blog8,
     date: "APRIL 05, 2018",
   },
 ]
